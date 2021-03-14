bc7b765a7b5b71e8963414bef36e154a54a17a12
SagnikKundu <69116169+SagnikKundu@users.noreply.github.com>
2021-03-14 10:24:45 +0530

    Create Heap Sort.py

---
+++ Heap Sort.py
@@ -0,0 +1,41 @@
+def heapify(arr, n, i): 
+    largest = i 
+    l = 2 * i + 1 
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+
+    # See if right child of root exists and is 
+    # greater than root 
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i] # swap 
+
+        # Heapify the root. 
+        heapify(arr, n, largest) 
+
+def heapSort(arr): 
+    n = len(arr) 
+    for i in range(n // 2 - 1, -1, -1): 
+        heapify(arr, n, i) 
+
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i] 
+        heapify(arr, i, 0) 
+
+arr = [2,1,12,11,13,5,6,7]
+print('Original Array is')
+print(*arr)
+heapSort(arr) 
+n = len(arr) 
+print("Length of the array is :",n)
+print ("Sorted array is") 
+for i in range(n-1,0,-1): 
+    print (arr[i],end = " ")
+print()
+for i in range(n): 
+    print (arr[i],end = " ")
